4f5882ca20339a05e1ac6e8303941305a7d3e01b
Dan Cheail <dan@undumb.com>
2013-05-31 11:37:31 +1000

    Revert "allow users to force an encoding for raw data before processing"
    This reverts commit 812802cd85ef238d9f612879e12f7e19e1704ac5.

--- lib/fencer.rb
+++ lib/fencer.rb
@@ -47,15 +47,10 @@ module Fencer
       end
     end
 
-    def initialize(raw_data, delimiter = nil, forced_encoding = nil)
-      @values          = {}
-      @delimiter       = delimiter
-
-      if forced_encoding
-        @raw_data = raw_data.encode(forced_encoding, invalid: :replace)
-      else
-        @raw_data = raw_data
-      end
+    def initialize(raw_data, delimiter = nil)
+      @values    = {}
+      @delimiter = delimiter
+      @raw_data  = raw_data
 
       parse!
     end
